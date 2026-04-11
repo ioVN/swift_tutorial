d53451cd004716da2c8b6f8106e16ad9cd468fb5
Mai Văn Thịnh <thinh@wee.vn>
2026-04-12 00:47:58 +0700

    Add Logo-ioVN image asset and update ContentView UI

--- Swift_Tutorial/ContentView.swift
+++ Swift_Tutorial/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 8) {
+            
+            // Use system images from SF Symboys
+            // Image(systemName: "house.fill")
+            //      .imageScale(.large)
+            //      .foregroundStyle(.tint)
+            //      .padding()
+            
+            //  Use images from the asset catalog, which can be added to your project in Xcode.
+            Image("Logo-ioVN")
+                .resizable()
+                .scaledToFit() // => same as .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .frame(width: 100, height: 100)
+            
+            HStack {
+                
+                Text("Hello, ioVN")
+                //  .font(.title3) <- shadow by HStack label
+                //  .fontWeight(.bold) <- shadow by HStack label
+                //  .foregroundStyle(.red) <- shadow by HStack label
+                
+                // Use `Spacer` to create flexible space between views in a layout.
+                // Spacer()
+                
+                Image(systemName: "wand.and.sparkles.inverse")
+                    .imageScale(.small) // => to small image
+                    .offset(x:0, y: -2) // => to move the image up by 2 points
+                //  .foregroundStyle(.red) <- shadow by HStack label
+            }
+            .font(.title3)
+            .fontWeight(.bold)
+            .foregroundStyle(.red)
+            
+            Text("Come visit for an exprience of lifetime...")
+            //  .frame(maxWidth: .infinity, alignment: .center)
         }
         .padding()
+        .preferredColorScheme(.light) // => to set the color scheme to light mode
     }
 }
 
